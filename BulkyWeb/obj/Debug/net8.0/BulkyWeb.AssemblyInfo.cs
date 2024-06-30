23d1bcb0a252dd8ac68f0049b45dc4cbbab6fc7a
Dhiraj Khapangi <dkhapangi13@outlook.com>
2024-06-30 16:37:48 +0545

    n-tier architecture

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+249c39e86d6654258fde4e5fff1f87b75a31eabc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c23e12c3dafc4bff9cced5bba726995f5dfb335")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
